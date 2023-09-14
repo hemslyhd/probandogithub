d9471ce684f210a96d2ab71ad04f5a8d0f852f30
hemslyhd <hhdiaz@uclv.cu>
2023-09-14 09:22:19 -0500

    Modifique el archivo de txt y cpp agregando un comentario del codigo

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main()
 {
     cout << "Hello World!" << endl;
     cout<<"Este es mi primera linea modificada en github"<<endl;
+    //agrego este comentario para ver como es que se modifican dos archivos distintos en git
     return 0;
 }
 
